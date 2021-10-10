847a31cb6396161d5b2ef9091cf84c5eee8dd0b5
gryphbecrazeh <gryphbecrazeh@gmail.com>
2021-10-10 02:46:58 +0100

    fixed issue from renaming methods and not unserializing main data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Verb;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,15 +12,19 @@ class DashboardController extends Controller
     public function __construct()
     {
     }
-    
+
     public function index()
     {
-        $strugglingWords = auth()->user()->learnedWords()->limit(5)->orderBy('timesWrong', 'desc')->get()->filter(function ($word) {
+        $strugglingWords = auth()->user()->learned_words()->limit(5)->orderBy('timesWrong', 'desc')->get()->filter(function ($word) {
             return $word->timesWrong;
         })->map(function ($word) {
-            return $word->getVerbObject();
+            $fetchedWord = collect($word->getVerbObject())->toArray();
+            $fetchedWord['meanings'] = \unserialize($fetchedWord['meanings']);
+            $fetchedWord['kanji'] = \unserialize($fetchedWord['kanji']);
+
+            return collect($fetchedWord);
         });
-        
-        return view('dashboard', ['strugglingWords'=>$strugglingWords]);
+
+        return view('dashboard', ['strugglingWords' => $strugglingWords]);
     }
 }
