c6a265bba965de6eb06ed12bb74b9eda6bed085b
gryphbecrazeh <gryphbecrazeh@gmail.com>
2021-10-10 17:13:06 +0100

    added gametype column to learned words table

---
+++ database/migrations/2021_10_10_154455_add_type_to_learned_words_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToLearnedWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('learned_words', function (Blueprint $table) {
+            //
+            $table->string('gametype')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('learned_words', function (Blueprint $table) {
+            //
+        });
+    }
+}
