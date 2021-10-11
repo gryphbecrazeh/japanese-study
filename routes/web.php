faf8ab80eb616424d567b57b81564969338703bc
gryphbecrazeh <gryphbecrazeh@gmail.com>
2021-10-11 03:20:15 +0100

    fixed routes, static routes for game type, general game route for redirecting based on game type

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\VerbGameController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KanjiGameController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\WordController;
 use App\Http\Controllers\Auth\LoginController;
@@ -18,6 +20,7 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::prefix('/auth')->group(function () {
     Route::prefix('/login')->group(function () {
         Route::get('/', [LoginController::class, 'index'])->name('auth.login');
@@ -50,38 +53,28 @@ Route::middleware(['auth'])->group(function () {
 
     Route::prefix('/game')->group(function () {
         Route::get('/', [GameController::class, 'index'])->name('game'); // GAME DASHBOARD, SCROLL THROUGH AVAILABLE GAMES
-    Route::get('/{game_type}', [GameController::class, 'index'])->name('game.verb.start'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
-    Route::get('/{game_type}/{game_id}', [GameController::class, 'index'])->name('game.verb.continue'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
-    Route::post('/{game_type}/{game_id}', [GameController::class, 'store'])->name('game.verb.post'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        Route::get('/start', [GameController::class, 'index'])->name('game.verb.start'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        // Route::get('/{game_type}/{game_id}', [GameController::class, 'index'])->name('game.verb.continue'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        // Route::post('/{game_type}/{game_id}', [GameController::class, 'store'])->name('game.verb.post'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        Route::get('/verb/{game_id}', [VerbGameController::class, 'index'])->name('game.verb.continue'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        Route::post('/verb/{game_id}', [VerbGameController::class, 'store'])->name('game.verb.post'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        Route::get('/kanji', [KanjiGameController::class, 'index'])->name('game.kanji.continue'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
+        Route::post('/kanji', [KanjiGameController::class, 'store'])->name('game.kanji.post'); // VERB OFFER NEW GAME OR CONTINUE GAME PRACTICE VERBS
     });
 
     Route::prefix('/user')->group(function () {
         Route::get('/', [UserController::class, 'index'])->name('user');
         Route::get('/profile', [UserController::class, 'index'])->name('user.profile'); // GET USER PROFILE DATA ( PERSONAL INFO, NAME, DATE OF BIRTH, RACE, NATIONALITY, GENDER )
-    Route::get('/account', [UserController::class, 'index'])->name('user.account'); // GET USER ACCOUNT DATA ( ACCOUNT INFO, USERNAME, EMAIL, PASSWORD )
-    Route::get('/support', [UserController::class, 'index'])->name('user.support'); // FOR REPORTING PROBLEMS, COMMENTS
-    Route::post('/logout', [UserController::class, 'exit'])->name('user.logout'); // DEAUTH USER
+        Route::get('/account', [UserController::class, 'index'])->name('user.account'); // GET USER ACCOUNT DATA ( ACCOUNT INFO, USERNAME, EMAIL, PASSWORD )
+        Route::get('/support', [UserController::class, 'index'])->name('user.support'); // FOR REPORTING PROBLEMS, COMMENTS
+        Route::post('/logout', [UserController::class, 'exit'])->name('user.logout'); // DEAUTH USER
     });
     Route::prefix('/report')->group(function () {
         Route::get('/{verb_id}', [WordController::class, 'report'])->name('report'); // Report word error
-
         Route::post('/{verb_id}', [WordController::class, 'report'])->name('report'); // Report word error
     });
 });
-// This is in the wrong place
-// Route::prefix('/api')->group(function () {
-//     Route::prefix('/word')->group(function () {
-//         Route::get('/word', [WordController::class, 'index'])->name('api.word'); // GET NEW WORD, UPDATE WORD GLOBAL STATS
-//         Route::put('/word/{word_id}', [WordController::class, 'index'])->name('api.word'); // UPDATE WORDS GLOBAL STATS
-//     });
-//     Route::prefix('/user')->group(function () {
-//         Route::put('/user/{user_id}', [UserController::class, 'index'])->name('api.user'); // UPDATE USER STATS
-//     });
-//     Route::prefix('/game')->group(function () {
-//         Route::get('/game/{user_id}', [GameController::class, 'index'])->name('api.user'); // LOAD OLD GAME, CREATE NEW GAME
-//         Route::put('/game/{user_id}', [GameController::class, 'index'])->name('api.user'); // UPDATE ACTIVE GAME, LEVEL, TOP SCORE, TOP STREAK
-//     });
-// });
+
 
 Route::get('/', function () {
     return view('home');
